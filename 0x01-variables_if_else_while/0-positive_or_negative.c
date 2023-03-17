ce8b5deff0499936e293bf27e4437f2a576fae10
MiracleAchebe <117732652+MiracleAchebe@users.noreply.github.com>
2023-03-17 23:33:37 +0100

    save of first file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,8 @@
 #include <studio.h>
 #include <stdlib.h>
   
- /* betty style doc for function main goes there */ 
+ /* betty style doc for function main goes there */
+ /*This code checks and prints if random number is zero, positive or negative*/
  int main(void) 
  { 
          int n; 
@@ -10,5 +11,12 @@
          srand(time(0)); 
          n = rand() - RAND_MAX / 2;
          /* your code goes there */
-         return (0); 
+         if (n == 0)
+         printf("is zero\n")
+         else if (n > 0)
+         printf("is positive"\n")
+         else
+         printf("is negative\n")
+         
+         return (0);
  }
