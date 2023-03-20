27c923a396361290f5b22398b29ccef786c82fb7
MiracleAchebe <nechemira2000@gmail.com>
2023-03-20 08:31:40 -0700

    corrected 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints "programming is like building a mul
+ * tilingual puzzle,
+ * Return : always zero
+ */
+int main(void)
+{
+	puts("\"programming is like building a multi
+	lingual puzzle");
+	return (0);
+}
